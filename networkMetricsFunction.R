0101d20baa519189269cf29568363d0aedd1de6c
NicoJaws23 <nico.jaworski@utexas.edu>
2025-04-11 08:58:36 -0500

    Metrics Function

---
+++ networkMetricsFunction.R
@@ -0,0 +1,33 @@
+#Network Mertric function
+library(tidyverse)
+library(igraph)
+
+#Takes 3 arguments, g, which is an igraph generated graph, 
+#cutoff which is the curoff value for the betweenness metric
+#and clusterType which is the clustering type for transitivity
+metrics <- function(g, cutoff, clusterType = c("undirected", "global", "globalundirected", "localundirected", "local", 
+"average", "localaverage", "localaverageundirected", "barrat", "weighted")){
+  inDegree <- igraph::degree(g, mode = "in")
+  outDegree <- igraph::degree(g, mode = "out")
+  inStrength <- igraph::strength(g, mode = "in")
+  outStrength <- igraph::strength(s1G, mode = "out")
+  betweenness <- igraph::betweenness(g, cutoff = -1)
+  eigenvectorCentrality <- as.data.frame(eigen_centrality(g))
+  clusteringCoefficient <- igraph::transitivity(s1G, type = clusterType)
+  
+  x <- data.frame(In.Degree = inDegree, Out.Degree = outDegree, In.Strength = inStrength, 
+                  Out.Strength = outStrength, Betweenness = betweenness, 
+                  Eigenvector.Centrality = eigenvectorCentrality$vector, Clustering.Coefficient = clusteringCoefficient)
+  
+  return(x)
+}
+
+#Testing out on data from Jaworski's data replication project
+f1 <- "https://raw.githubusercontent.com/NicoJaws23/dataReplication/refs/heads/main/GroomDyads_S1.csv"
+s1 <- read_csv(f1)
+head(s1)
+
+s1Edges <- data.frame(from = s1$"Actor", to = s1$"Recip", weight = s1$groom.rate)
+s1G <- graph_from_data_frame(d = s1Edges, directed = TRUE)
+
+met <- metrics(s1G, cutoff = -1, clusterType = "local")
