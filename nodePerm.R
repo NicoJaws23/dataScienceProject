6e0913deddcb1dd61cfdc5ba36ccb9d8b6b0c76e
NicoJaws23 <nico.jaworski@utexas.edu>
2025-04-16 12:57:38 -0500

    multi-species network

--- nodePerm.R
+++ nodePerm.R
@@ -5,39 +5,61 @@ library(lme4)
 library(mosaic)
 
 nodePerm <- function(df, shuffler, formula, n, type = c("rpt", "rptPoisson", "GLMM", "GLMMpoisson"), grname, na.action, returnVal){
-  permN <- do(n) * {
+  Obs_est <- NA
+  permN <- numeric(n)
+    
+  if(type == "rpt"){
+  m <- rptR::rpt(data = df, formula = formula, grname = grname, datatype = "Gaussian", na.action = na.action)
+  Obs_est <- m$R[[returnVal]]
+  permN <- mosaic::do(n) * {
     permd <- df
-    permd[[shuffler]] = sample(permd[[shuffler]]) 
-    
-    perm_est <- NA
-    
-    if(type == "rpt"){
-    m <- rpt(data = permd, formula = formula, grname = grname, datatype = "Gaussian")
-    perm_est <- m$R[[returnVal]]
+    permd[[shuffler]] = mosaic::sample(permd[[shuffler]])
+    p <- rptR::rpt(data = permd, formula = formula, grname = grname, datatype ="Gaussian", na.action = na.action)
+    p$R[[returnVal]]
     }
-    else if(type == "rptPoisson"){
-    m <- rpt(data = permd, formula = formula, grname)
-    perm_est <- m$R[[returnVal]]
-    }
-    else if(type == "GLMM"){
-    m <- lmer(data = permd, formula = formula, na.action = na.action)
-    perm_est <- fixef(m)[[returnVal]]
-    }
-    else if(type == "GLMMpoisson"){
-    m <- glmer(data = permd, formula = formula, family = poisson)
-    perm_est <- fixef(m)[[returnVal]]
-    }
-    data.frame(perm_est = perm_est)
   }
-  return(permN)
+    
+    
+  else if(type == "rptPoisson"){
+  m <- rptR::rptPoisson(data = df, formula = formula, grname, na.action = na.action)
+  Obs_est <- m$R[[returnVal]]
+  permN <- mosaic::do(n) * {
+    permd <- df
+    permd[[shuffler]] = mosaic::sample(permd[[shuffler]])
+    p <- rptR::rptPoisson(data = permd, formula = formula, grname, na.action = na.action)
+    p$R[[returnVal]]
+    }
+  }
+  
+  else if(type == "GLMM"){
+  m <- lme4::lmer(data = df, formula = formula, na.action = na.action)
+  Obs_est <- lme4::fixef(m)[[returnVal]]
+  permN <- mosaic::do(n) * {
+    permd <- df
+    permd[[shuffler]] = mosaic::sample(permd[[shuffler]])
+    p <- lme4::lmer(data = permd, formula = formula, na.action = na.action)
+    lme4::fixef(p)[[returnVal]]
+  }
+  }
+  
+  else if(type == "GLMMpoisson"){
+  m <- lme4::glmer(data = df, formula = formula, family = poisson, na.action = na.action)
+  Obs_est <- lme4::fixef(m)[[returnVal]]
+  permN <- mosaic::do(n) * {
+    permd <- df
+    permd[[shuffler]] = mosaic::sample(permd[[shuffler]])
+    p <- lme4::glmer(data = permd, formula = formula, family = poisson, na.action = na.action)
+    lme4::fixef(p)[[returnVal]]
+  }
+  }
+  
+  return(list(observed = Obs_est, permuted = permN))
 }
-
+  
 
 
 c <- nodePerm(df = df, shuffler = "Sex", formula = In.Degree ~ (1|ID) + Sex, n = 1000, type = "GLMM", returnVal = "SexM", na.action = na.exclude)
 
+#network with focals and humans
 
-
-
-
-
+#create graph function for complex networks, with node specific attributes
